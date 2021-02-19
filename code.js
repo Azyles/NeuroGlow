ba4526baccd2ea9592dc968f002844cf38d757b9
Kushagra Singh <kushthaksh@gmail.com>
2021-02-19 09:20:17 -0800

    Sizing Effect

--- code.js
+++ code.js
@@ -55,3 +55,19 @@ $(document).ready(function() {
 });
 
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.height = "87%";
+    document.getElementById("header").style.width = "90%";
+    document.getElementById("header").style.marginLeft = "5%";
+    document.getElementById("header").style.marginRight = "5%";
+  } else {
+    document.getElementById("header").style.height = "100%";
+    document.getElementById("header").style.width = "100%";
+    
+    document.getElementById("header").style.marginLeft = "0";
+    document.getElementById("header").style.marginRight = "0";
+  }
+} 
